40a5728e4305d707188d0d74e010cc3c04d93687
RuqiyaG <ruqiyageelle@gmail.com>
2025-12-01 20:24:41 +0000

    starting index.js by importing and setting up basics and made a css file

---
+++ index.js
@@ -0,0 +1,22 @@
+// importing all neccessary components e.g sql.express,express session and sanitizer
+var express = require('express');
+var ejs = require('ejs');
+var mysql = require('mysql2');
+var session = require('express-session');
+const path = require('path');
+const expressSanitizer = require('express-sanitizer');
+require('dotenv').config();
+
+// creating the express app
+const app = express();
+const port = 8000;
+
+// using ejs as a template
+app.set('view engine', 'ejs');
+// body parser set up
+app.use(express.urlencoded({extended: true}));
+// connecting css file so that it can be used
+app.use(express.static(path.join(__dirname, 'style')));
+// setting up data to use for my fitness app
+app.locals.shopData = {shopName: "Fitness Galore"}
+
