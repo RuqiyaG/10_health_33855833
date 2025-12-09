2921ed3043cc2127d83547e7be8699eaf52aafe2
RuqiyaG <ruqiyageelle@gmail.com>
2025-12-09 11:58:20 +0000

    made routes for home and about

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,14 @@
 const express = require("express")
 const router = express.Router()
 
+// handling routes
+router.get('/',function(req, res, next){
+    res.render('index.ejs')
+});
 
+router.get('/about',function(req, res, next){
+    res.render('about.ejs')
+});
 
 
 
