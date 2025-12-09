dd4d80f854349ba17ebe3cbed786f75f4f5845bc
RuqiyaG <ruqiyageelle@gmail.com>
2025-12-09 15:51:27 +0000

    fixing

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,8 @@
-// creating new rouer and importing express to use
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/searching',function(req, res, next){
+router.get('/search', function(req, res, next) {
     res.render('search.ejs')
 });
 
-// export router so index.js can accsess it
 module.exports = router
