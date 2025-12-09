145860b05f118bdee2c8035f447f17ab133b3643
RuqiyaG <ruqiyageelle@gmail.com>
2025-12-09 11:54:54 +0000

    created basic database and layout for routes

---
+++ create_db.sql
@@ -0,0 +1,26 @@
+# Create database script for Berties books
+
+# Create the database
+CREATE DATABASE IF NOT EXISTS health;
+USE health;
+
+# Create the tables
+
+CREATE TABLE IF NOT EXISTS users (
+    id               INT AUTO_INCREMENT,
+    username         VARCHAR(255) NOT NULL UNIQUE,
+    first            VARCHAR(255) NOT NULL,
+    last             VARCHAR(255) NOT NULL,
+    email            VARCHAR(255) NOT NULL UNIQUE,  
+    hashedPassword   VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id));
+
+CREATE TABLE IF NOT EXISTS appointments (
+    id               INT AUTO_INCREMENT,
+    appDate          DATETIME NOT NULL,
+    appInfo          TEXT
+    PRIMARY KEY(id));
+    
+
+CREATE USER IF NOT EXISTS 'health_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
+GRANT ALL PRIVILEGES ON berties_books.* TO ' health_app'@'localhost';
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+// creating new rouer and importing express to use
+const express = require("express")
+const router = express.Router()
+
+
+
+
+
+
+// export router so index.js can accsess it
+module.exports = router
